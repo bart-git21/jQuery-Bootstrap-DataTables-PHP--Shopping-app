e5a92a5040cf37f77c07532c887baf08198dacd6
gitName <bartk@list.ru>
2025-01-31 22:38:56 +0300

    refactor: use the ROOT_PATH constant instead of url_for function

--- client/pages/header.php
+++ client/pages/header.php
@@ -1,17 +1,19 @@
 <?php
-function url_for($page_path): string
+// require "../../config.php";
+function getRootPath():string
 {
-    $base_url = 'http://' . $_SERVER['HTTP_HOST'];
-    $project_folder = 'shopping';
-    $project_pos = strpos($_SERVER['REQUEST_URI'], $project_folder);
-    $base_path = substr($_SERVER['REQUEST_URI'], 0, $project_pos + strlen($project_folder));
-    return $base_url . $base_path . $page_path;
+    $configFilePath = parse_url(__FILE__)["path"];
+    $rootDirPos = strrpos($configFilePath, "client");
+    $directoryPath = substr_replace($configFilePath, "", $rootDirPos, strlen($configFilePath));
+    return str_replace("\\", "/", $directoryPath);
 }
+$path = getRootPath(); 
+define("ROOT_PATH", "http:/" .getRootPath());
 ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
-            <img src="<?php echo url_for('/client/public/images/glasses-logo.png') ?>" alt="">
+            <img src="<?php echo ROOT_PATH . 'client/public/images/glasses-logo.png'; ?>" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,10 +23,10 @@ function url_for($page_path): string
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page"
-                        href="<?php echo url_for('/client/index.php'); ?>">Home</a>
+                        href="<?php echo ROOT_PATH . '/client/index.php'; ?>">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo url_for('/client/pages/price.php'); ?>">Price trend</a>
+                    <a class="nav-link" href="<?php echo ROOT_PATH . '/client/pages/price.php'; ?>">Price trend</a>
                 </li>
             </ul>
         </div>
