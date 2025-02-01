2242127c7223b9ccffb8f899f976a38d848a2b2e
gitName <bartk@list.ru>
2025-02-01 13:57:28 +0300

    feat: rewrite the product data as an object; data for dataTables lib is in the tableData key

--- client/src/model.js
+++ client/src/model.js
@@ -1,16 +1,23 @@
 const filesNames = [1, 2];
-// const filesNames = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-async function getSingleFileData(url) {
-  const response = await fetch(`./db/html/${url}.txt`);
-  const data = await response.text();
-  const products = data.split("data-index");
+// const filesNames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+async function getHtml(fileName) {
+  const response = await fetch(`db/html/${fileName}.txt`);
+  return await response.text();
+}
+async function getSingleFileData(fileName) {
   let splitedProducts = [];
+
+  const data = await getHtml(fileName);
+  const products = data.split("data-index");
   products.forEach((e) => {
     e = e.replaceAll("> <", "><");
     const elem = e.split("><");
 
     if (elem.length > 3) {
-      const href = elem.find(e => e.includes("href")).split(" ")[1].split("/product")[1];
+      const href = elem
+        .find((e) => e.includes("href"))
+        .split(" ")[1]
+        .split("/product")[1];
       let id = href.replace(/(\/\?.{1,})/g, "");
       id = id.replace(/(.{1,}-)/g, "");
       const img = elem
@@ -32,17 +39,53 @@ async function getSingleFileData(url) {
       rateAmount &&
         (rateAmount = rateAmount.split(">")[1].replaceAll(/\D/g, ""));
 
-      const productParameters = [
-        +rateAmount,
-        `<a href='https://www.example.com/product${href}' target="_blank" rel="noopener noreferrer"><img class='link' ${img} alt='${name}'></a>`,
-        `<a href='https://www.example.com/product${href}' target="_blank" rel="noopener noreferrer">${name}</a>`,
-        id,
-        +priceExample,
-        parseFloat(rate),
-        name,
-      ];
+      const numbersGr = name.replaceAll(/ {1,}/g, "").match(/(\d+(г){1,2})/g);
+      const numbersKg = name.replaceAll(/ {1,}/g, "").match(/(\d+(кг){1,2})/g);
+      let pricePerKg = [["", 99999]];
+      if (numbersGr || numbersKg) {
+        pricePerKg = numbersGr
+          ? numbersGr.map((e) => [e, (priceExample / parseInt(e)) * 1000])
+          : numbersKg.map((e) => [e, priceExample / parseInt(e)]);
+      }
+      const numbersMl = name.replaceAll(/ {1,}/g, "").match(/(\d+(мл){1,2})/g);
+      const numbersLitr = name.replaceAll(/ {1,}/g, "").match(/(\d+(л){1,2})/g);
+      let pricePerLitr = [["", ""]];
+      if (numbersMl || numbersLitr) {
+        pricePerLitr = numbersMl
+          ? numbersMl.map((e) => [e, (priceExample / parseInt(e)) * 1000])
+          : numbersLitr.map((e) => [e, priceExample / parseInt(e)]);
+      }
 
-      splitedProducts.push(productParameters);
+      const prodParam = {
+        id,
+        feedbacks: +rateAmount,
+        href,
+        name,
+        price: +priceExample,
+        rate: parseFloat(rate),
+        tableData: [
+          +rateAmount,
+          `<a href='https://www.example.com/product${href}' target="_blank" rel="noopener noreferrer"><img class='link' ${img} alt='${name}'></a>`,
+          `<a href='https://www.example.com/product${href}' target="_blank" rel="noopener noreferrer">${name}</a>`,
+          id,
+          +priceExample,
+          `
+                <table>
+                    <tr>
+                        <td>${parseInt(pricePerKg[0][1])} р/кг</td>
+                        <td>${pricePerKg[0][0]}</td>
+                    </tr>
+                    <tr ${!pricePerLitr[0][1] && "hidden"}>
+                        <td>${parseInt(pricePerLitr[0][1])} р/л</td>
+                        <td>${pricePerLitr[0][0]}</td>
+                    </tr>
+                </table>
+            `,
+          parseFloat(rate),
+        ],
+      };
+
+      priceExample && splitedProducts.push(prodParam);
     }
   });
   return splitedProducts;
@@ -60,42 +103,54 @@ async function getData() {
 
 class Products {
   constructor(list) {
-    this.list = list || [];
+    this.list = [...list] || [];
+    this.tableData = list.map((e) => e.tableData) || [];
   }
   async create() {
     this.list = await getData();
+    this.tableData = this.list.map((e) => e.tableData);
   }
   async read() {
     if (!this.list.length) await this.create();
-    return this.list;
+    return this.tableData;
   }
   everyFilter(condition) {
     const words = condition.toLowerCase().split(" ");
-    return this.list.filter((e) => {
-      const name = e[2].toLowerCase();
+    const filteredList = this.list.filter((e) => {
+      const name = e.name.toLowerCase();
       const isAllIncludes = words.every((word) => name.includes(word));
       return !isAllIncludes;
     });
+    this.tableData = filteredList.map(e => e.tableData);
+    return this.tableData;
   }
   someFilter(condition) {
     const words = condition.toLowerCase().split(" ");
-    return this.list.filter((e) => {
-      const name = e[6].toLowerCase();
+    const filteredList = this.list.filter((e) => {
+      const name = e.name.toLowerCase();
       const isIncludesAtListOneWord = words.some((word) => name.includes(word));
       return !isIncludesAtListOneWord;
     });
+    this.tableData = filteredList.map(e => e.tableData);
+    return this.tableData;
   }
   unique() {
-    const uniqueRate = new Set(this.list.map((e) => e[0]));
-    return [...uniqueRate].map((rate) =>
-      this.list.find((product) => product[0] === rate)
+    const uniqueRate = new Set(this.list.map((e) => e.feedbacks));
+    const filteredList = [...uniqueRate].map((rate) =>
+      this.list.find((product) => product.feedbacks === rate)
     );
+    this.tableData = filteredList.map(e => e.tableData);
+    return this.tableData;
   }
   rateFilter(num) {
-    return this.list.filter((e) => e[5] >= num);
+    const filteredList = this.list.filter((e) => e.rate >= num);
+    this.tableData = filteredList.map(e => e.tableData);
+    return this.tableData;
   }
   rateAmountFilter(num) {
-    return this.list.filter((e) => e[0] >= num);
+    const filteredList = this.list.filter((e) => e.feedbacks >= num);
+    this.tableData = filteredList.map(e => e.tableData);
+    return this.tableData;
   }
 }
 const model = new Products(await getData());
