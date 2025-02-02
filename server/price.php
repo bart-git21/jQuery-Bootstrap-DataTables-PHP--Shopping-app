5a0618baa428a519cf227a8d13fc642059bde524
gitName <bartk@list.ru>
2025-02-02 19:48:48 +0300

    feat: save the discount list into json

--- server/price.php
+++ server/price.php
@@ -8,7 +8,7 @@ $todayPrice = json_decode($post);
 
 // Update price history in the JSON file
 $priceHistory = json_decode(file_get_contents("db/price.json"));
-$changedProducts = [];
+$discountList = [];
 foreach ($todayPrice as $key => $newProduct) {
     // find product in the history list that matches the current product
     $index = array_search($newProduct->id, array_column($priceHistory, 'id'));
@@ -17,7 +17,8 @@ foreach ($todayPrice as $key => $newProduct) {
     if ($index) {
         // if the price is different, add it to the price history
         if (intval(end($priceHistory[$index]->price)) !== intval($newProduct->price)) {
-            (intval(end($priceHistory[$index]->price)) > intval($newProduct->price)) && array_push($changedProducts, $priceHistory[$index]);
+            (intval(end($priceHistory[$index]->price)) > intval($newProduct->price)) && array_push($discountList, $priceHistory[$index]);
+            file_put_contents("db/discount.json", json_encode($discountList, JSON_UNESCAPED_UNICODE));
             array_push($priceHistory[$index]->price, $newProduct->price);
         }
     } else {
@@ -29,4 +30,4 @@ foreach ($todayPrice as $key => $newProduct) {
 file_put_contents("db/price.json", json_encode($priceHistory, JSON_UNESCAPED_UNICODE));
 
 // Return the updated price data
-echo json_encode($changedProducts);
+echo json_encode(["status" => "success"]);
