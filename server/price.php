16ad3b09ae380c1fe7ed3923209a71f786b8eb53
gitName <bartk@list.ru>
2025-02-02 19:40:12 +0300

    feat: send to frontend the discounted price only

--- server/price.php
+++ server/price.php
@@ -8,6 +8,7 @@ $todayPrice = json_decode($post);
 
 // Update price history in the JSON file
 $priceHistory = json_decode(file_get_contents("db/price.json"));
+$changedProducts = [];
 foreach ($todayPrice as $key => $newProduct) {
     // find product in the history list that matches the current product
     $index = array_search($newProduct->id, array_column($priceHistory, 'id'));
@@ -16,6 +17,7 @@ foreach ($todayPrice as $key => $newProduct) {
     if ($index) {
         // if the price is different, add it to the price history
         if (intval(end($priceHistory[$index]->price)) !== intval($newProduct->price)) {
+            (intval(end($priceHistory[$index]->price)) > intval($newProduct->price)) && array_push($changedProducts, $priceHistory[$index]);
             array_push($priceHistory[$index]->price, $newProduct->price);
         }
     } else {
@@ -27,4 +29,4 @@ foreach ($todayPrice as $key => $newProduct) {
 file_put_contents("db/price.json", json_encode($priceHistory, JSON_UNESCAPED_UNICODE));
 
 // Return the updated price data
-echo json_encode(["status" => "success"]);
+echo json_encode($changedProducts);
