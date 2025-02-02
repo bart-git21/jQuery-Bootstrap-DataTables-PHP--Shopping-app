70b307526a2b97bc94bc4bd725312266ed8a1f82
gitName <bartk@list.ru>
2025-02-02 18:03:54 +0300

    feat: put today prices into file

--- server/price.php
+++ server/price.php
@@ -1,4 +1,7 @@
 <?php
-header("contentType: application/json");
+header("Content-Type: application/json");
 $post = file_get_contents("php://input");
+
+// // Update today's price in the JSON file
 file_put_contents("db/todayPrice.json", $post);
+$todayPrice = json_decode($post);
