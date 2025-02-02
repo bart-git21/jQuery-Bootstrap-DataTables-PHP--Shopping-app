702709674d873498838f9c3116ec70ddd8ad3fde
gitName <bartk@list.ru>
2025-02-02 18:06:20 +0300

    feat: update price using today data

--- server/price.php
+++ server/price.php
@@ -11,4 +11,14 @@ $priceHistory = json_decode(file_get_contents("db/price.json"));
 foreach ($todayPrice as $key => $newProduct) {
     // find product in the history list that matches the current product
     $index = array_search($newProduct->id, array_column($priceHistory, 'id'));
+
+    // update product price in the history list
+    if ($index) {
+        array_push($aa, $priceHistory[$index]->price);
+        array_push($priceHistory[$index]->price, $newProduct->price);
+    } else {
+        $newProduct->price = [$newProduct->price];
+        array_push($priceHistory, $newProduct);
+    }
+    ;
 }
