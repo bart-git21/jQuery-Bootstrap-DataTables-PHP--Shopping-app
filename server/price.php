a6aed44c822382b1c7bbcd3e2cd3c12123114fcf
gitName <bartk@list.ru>
2025-02-02 18:05:18 +0300

    feat: get price list from the file

--- server/price.php
+++ server/price.php
@@ -5,3 +5,10 @@ $post = file_get_contents("php://input");
 // // Update today's price in the JSON file
 file_put_contents("db/todayPrice.json", $post);
 $todayPrice = json_decode($post);
+
+// Update price history in the JSON file
+$priceHistory = json_decode(file_get_contents("db/price.json"));
+foreach ($todayPrice as $key => $newProduct) {
+    // find product in the history list that matches the current product
+    $index = array_search($newProduct->id, array_column($priceHistory, 'id'));
+}
